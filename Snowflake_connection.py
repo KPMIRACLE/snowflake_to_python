8a77648d92b152ba6855fe21a8cf58513dadaeb9
KPMIRACLE <149605614+KPMIRACLE@users.noreply.github.com>
2023-11-02 16:06:15 -0500

    THis is my inital commit

--- Snowflake_connection.py
+++ Snowflake_connection.py
@@ -1,1 +1,8 @@
-
+import snowflake.connector as SF
+connection = SF.connect(user ='Kiranmiracle7',
+                        password = 'Thankyou@2023',
+                        account = 'cqyemzs-nd52009')
+connection.Cursor().execute("CREATE OR REPLACE DATABASE TRANSFORMATIONS_DEV_DB")
+connection.Cursor().execute("CREATE OR REPLACE SCHEMA TRANSFORMATIONS_DEV_SC")
+connection.Cursor().execute("CREATE OR REPLACE TABLE EMPLOYEE(E_ID INTERGER, E_NAME VARCHAR, E_COMPANY VARCHAR, E_ROLE VARCHAR)")
+connection.close()
